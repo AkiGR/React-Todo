f626d26b594ea130001cffc2bca3980bb9c7707f
AkiGR <hanauchi.aki@gmail.com>
2024-03-17 14:54:04 +0900

    add Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// style
+import { Center } from "@chakra-ui/react";
+
 export const Header: React.FC = () => {
-  return <>AkiGR</>;
+  return (
+    <>
+      <Center fontSize="1.5rem" color="#81A1C1">
+        AkiGR
+      </Center>
+    </>
+  );
 };
