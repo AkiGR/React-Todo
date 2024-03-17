9d105d236e7a982a30256722918b1c13934742eb
AkiGR <hanauchi.aki@gmail.com>
2024-03-17 15:05:59 +0900

    add todo form

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+// style
+import { Input } from "@chakra-ui/input";
+import { Button } from "@chakra-ui/button";
+
 export const TodoForm: React.FC = () => {
-  return <>TodoForm</>;
+  return (
+    <>
+      <Input placeholder="Todo title" />
+      <Button color="#81A1C1">Add todo</Button>
+    </>
+  );
 };
